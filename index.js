f1b035476aa362fff0775ee89b886f2dd901b03e
Nathan Friedly <nathan@nfriedly.com>
2016-02-09 17:51:06 -0500

    added short preamble to index.js + automatic lib.version on travis builds

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+// IBM Watson Speech JavaScript SDK
+// Copyright IBM (Apache-2.0)
+
 'use strict';
+
+exports.version = process.env.TRAVIS_BRANCH;
 
 exports.SpeechToText = require('./speech-to-text');
 
-// todo: add a text-to-speech library next
+// todo: add text-to-speech
