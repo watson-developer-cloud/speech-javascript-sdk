b164b46aac8d0c16e426b7d9fe3c02004b31d4a9
Mike Reinstein <reinstein.mike@gmail.com>
2017-01-28 21:09:43 -0800

    enable passing a userMediaStream as an option

--- speech-to-text/recognize-microphone.js
+++ speech-to-text/recognize-microphone.js
@@ -94,7 +94,11 @@ module.exports = function recognizeMicrophone(options) {
     preservedMicStream.unpipe(bitBucket);
     getMicStream = Promise.resolve(preservedMicStream);
   } else {
-    getMicStream = getUserMedia({video: false, audio: true}).then(function(mic) {
+    var pm = options.userMediaStream ?
+      Promise.resolve(options.userMediaStream) : 
+      getUserMedia({video: false, audio: true})
+
+    getMicStream = pm.then(function(mic) {
       var micStream = new MicrophoneStream(mic, {
         objectMode: true,
         bufferSize: options.bufferSize
