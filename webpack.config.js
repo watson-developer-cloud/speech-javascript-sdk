1a01e00e1b80695b096bc8d8382a198c633584f1
Nathan Friedly <nathan@nfriedly.com>
2017-02-10 08:24:48 -0500

    lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,14 @@
 // note: this isn't used at the moment
 // browserify output is slightly smaller, but they're within 1kb of eachother after minifying and gzipping
 module.exports = {
-  entry: "./index.js",
+  entry: './index.js',
   output: {
-    path: __dirname + "/dist",
-    filename: "watson-speech.js",
-    library: "WatsonSpeech",
-    libraryTarget: "umd"
+    path: __dirname + '/dist',
+    filename: 'watson-speech.js',
+    library: 'WatsonSpeech',
+    libraryTarget: 'umd'
   },
   module: {
-    loaders: [
-      { test: /index.js$/, loader: "envify-loader" }
-    ]
+    loaders: [{ test: /index.js$/, loader: 'envify-loader' }]
   }
 };
