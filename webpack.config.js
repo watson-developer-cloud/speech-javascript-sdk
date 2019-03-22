8d70606a02b29dbabd20debf252065b73a80853b
German Attanasio <germanattanasio@gmail.com>
2019-03-22 11:33:58 -0400

    fix: Update karma and add babelify to browserify

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,15 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
         test: /index.js$/,
-        loader: `transform-loader?envify`
+        loader: 'transform-loader?envify'
         // options: {...}
       }
     ]
